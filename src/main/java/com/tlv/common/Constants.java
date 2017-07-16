59243113bcbc0af6eda162ba29e0e6cae07ff727
Keerthi <k.keerthi71@yahoo.com>
2017-07-17 01:17:11 +0530

    Added new Error constant messages

--- src/main/java/com/tlv/common/Constants.java
+++ src/main/java/com/tlv/common/Constants.java
@@ -10,6 +10,8 @@ public class Constants {
 	public static final String INVALID_LENGTH = "LENGTH not valid";
 	public static final String INVALID_LENGTH_FORMAT = "LENGTH is not of 4 chars";
 	public static final String INVALID_VALUE = "VALUE not valid";
+	public static final String INVALID_VALUE_LENGTH = "VALUE size is less than LENGTH";
 	public static final String INVALID_FORMAT = "Input is in invalid Format";
+	public static final String UNKNOWN_STRATEGY = "Unkonwn Strategy";
 
 }
